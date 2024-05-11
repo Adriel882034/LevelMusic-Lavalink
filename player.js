333c03c07dba7a979ba7f281d42f778e3748742a
Adriel882034 <152195399+Adriel882034@users.noreply.github.com>
2024-05-11 13:59:46 -0300

    player.js

--- player.js
+++ player.js
@@ -63,37 +63,37 @@ function initializePlayer(client) {
             iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1236664581364125787/music-play.gif?ex=6638d524&is=663783a4&hm=5179f7d8fcd18edc1f7d0291bea486b1f9ce69f19df8a96303b75505e18baa3a&', 
             url: 'https://discord.gg/xQF9f9yUEM'
         })
-        .setDescription(`➡️ **Song Name:** [${track.info.title}](${track.info.uri})\n➡️ **Author:** ${track.info.author}\n➡️ **Platforms :** YouTube, Spotify, SoundCloud`)
+        .setDescription(`➡️ **Nome da Música:** [${track.info.title}](${track.info.uri})\n➡️ **Autor:** ${track.info.author}\n➡️ **Plataformas :** YouTube, Spotify, SoundCloud`)
 
         .setImage(`https://cdn.discordapp.com/attachments/1004341381784944703/1165201249331855380/RainbowLine.gif?ex=663939fa&is=6637e87a&hm=e02431de164b901e07b55d8f8898ca5b1b2832ad11985cecc3aa229a7598d610&`)
         .setThumbnail(track.info.thumbnail)
         .setTimestamp()
-        .setFooter({ text: 'Click below buttons to control playback!'}); 
+        .setFooter({ text: 'Clique nos botões abaixo para controlar a reprodução !'}); 
 
 
 
         const queueLoopButton = new ButtonBuilder()
             .setCustomId("loopQueue")
-            .setLabel("Loop Queue")
+            .setLabel("Loop na Fila")
             .setStyle(ButtonStyle.Primary);
 
         const disableLoopButton = new ButtonBuilder()
             .setCustomId("disableLoop")
-            .setLabel("Disable Loop")
+            .setLabel("Desativar Loop")
             .setStyle(ButtonStyle.Primary);
 
         const skipButton = new ButtonBuilder()
             .setCustomId("skipTrack")
-            .setLabel("Skip")
+            .setLabel("Pular")
             .setStyle(ButtonStyle.Success);
 
         const showQueueButton = new ButtonBuilder()
             .setCustomId("showQueue")
-            .setLabel("Show Queue")
+            .setLabel("Mostrar Fila")
             .setStyle(ButtonStyle.Primary);
         const clearQueueButton = new ButtonBuilder()
             .setCustomId("clearQueue")
-            .setLabel("Clear Queue")
+            .setLabel("Limpar Fila")
             .setStyle(ButtonStyle.Danger);
 
     
@@ -126,12 +126,12 @@ function initializePlayer(client) {
                 setLoop(player, 'queue');
                 const loopEmbed = new EmbedBuilder()
             .setAuthor({
-                    name: 'Queue Loop!',
+                    name: 'Loop iniciado!',
                     iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157318080670728283/7905-repeat.gif?ex=66383bb4&is=6636ea34&hm=65f37cf88245f1c09285b547fda57b82828b3bbcda855e184f446d6ff43756b3&', 
                     url: 'https://discord.gg/xQF9f9yUEM'
                 })
             .setColor("#00FF00")
-            .setTitle("**Queue loop is Activated!**")
+            .setTitle("**Loop de fila foi Ativado!**")
          
 
         await channel.send({ embeds: [loopEmbed] });
@@ -140,11 +140,11 @@ function initializePlayer(client) {
                 const skipEmbed = new EmbedBuilder()
                 .setColor('#3498db')
                 .setAuthor({
-                  name: 'Song Skipped',
+                  name: 'Música pulada',
                   iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157269773118357604/giphy.gif?ex=6517fef6&is=6516ad76&hm=f106480f7d017a07f75d543cf545bbea01e9cf53ebd42020bd3b90a14004398e&',
                   url: 'https://discord.gg/FUEHs7RCqz'
                 })
-            .setTitle("**Player will play the next song!**")
+            .setTitle("**Tocando a próxima música!**")
             .setTimestamp();
                
     
@@ -154,11 +154,11 @@ function initializePlayer(client) {
                 const loopEmbed = new EmbedBuilder()
                 .setColor("#0099ff")
                 .setAuthor({
-                    name: 'Looping Off',
+                    name: 'Loop desativado',
                     iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230836684774576168/7762-verified-blue.gif?ex=6638b97d&is=663767fd&hm=021725868cbbc66f35d2b980585489f93e9fd366aa57640732dc49e7da9a80ee&', 
                     url: 'https://discord.gg/xQF9f9yUEM'
                 })
-                .setDescription('**Loop is Disabled for queue and single Song!**');
+                .setDescription('**Loop está desabilitado para fila e música única!**');
                   
     
             await channel.send({ embeds: [loopEmbed] });
@@ -168,7 +168,7 @@ const pageSize = 10;
 
 const queueMessage = queueNames.length > 0 ?
     queueNames.map((song, index) => `${index + 1}. ${song}`).join('\n') :
-    "The queue is empty.";
+    "A fila está vazia.";
 
 
 const pages = [];
@@ -182,7 +182,7 @@ for (let i = 0; i < pages.length; i++) {
 
     const queueEmbed = new EmbedBuilder()
         .setColor("#0099ff")
-        .setTitle(`Current Queue (Page ${i + 1}/${pages.length})`)
+        .setTitle(`Fila Atual (Page ${i + 1}/${pages.length})`)
         .setDescription(numberedSongs);
 
     await channel.send({ embeds: [queueEmbed] });
@@ -193,11 +193,11 @@ for (let i = 0; i < pages.length; i++) {
                 const queueEmbed = new EmbedBuilder()
                 .setColor("#0099ff")
                 .setAuthor({
-                    name: 'Queue Cleared',
+                    name: 'Fila Limpa',
                     iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230836684774576168/7762-verified-blue.gif?ex=6638b97d&is=663767fd&hm=021725868cbbc66f35d2b980585489f93e9fd366aa57640732dc49e7da9a80ee&', 
                     url: 'https://discord.gg/xQF9f9yUEM'
                 })
-                .setDescription('**Queue Songs cleared sucessfully!**');
+                .setDescription('**Fila de músicas apagadas com sucesso!**');
                
     
             await channel.send({ embeds: [queueEmbed] });
@@ -219,7 +219,7 @@ for (let i = 0; i < pages.length; i++) {
             player.destroy();
             const queueEmbed = new EmbedBuilder()
                 .setColor("#0099ff")
-                .setDescription('**Queue Songs ended! Disconnecting Bot!**');
+                .setDescription('**A Fila acabou, então eu saí!**');
                
     
             await channel.send({ embeds: [queueEmbed] });
